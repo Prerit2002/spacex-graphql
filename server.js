ad4619b411ad90e916ecb9d3b459f7e151252d04
Prerit2002 <Prerit715@gmail.com>
2021-07-25 01:17:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema')
+
+const app = express()
+
+
+
+
+app.use('/graphql', graphqlHTTP({
+    schema:schema,
+    graphiql:true
+}))
+
+app.listen(4000, () => {
+    console.log('listening on port 4000')
+})
